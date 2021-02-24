7be6c84442f39a946c66226deffd4f1113c7ea4a
Daniel Heinze <daheinze@microsoft.com>
2021-02-24 16:56:41 +0100

    feat: update setup.py for package publish

--- setup.py
+++ setup.py
@@ -10,19 +10,31 @@ def read(file_name):
     return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 
+# https://packaging.python.org/guides/distributing-packages-using-setuptools/#choosing-a-versioning-scheme
+MAJOR = "0"  # version when they make incompatible API changes
+MINOR = "1"  # version when they add functionality in a backwards-compatible manner
+MAINTENANCE = "5"  # version when they make backwards-compatible bug fixes.
+RELEASE_TYPE = "a1"
+
 setup(
     name="flask-stub-server",
-    version="0.0.1",
+    version=f"{MAJOR}.{MINOR}.{MAINTENANCE}{RELEASE_TYPE}",
     author="Daniel Heinze",
     author_email="daniel.heinze@microsoft.com",
-    description=("Provides the user with a Docker container, "
-                 "that exposes endpoints, which can be configured via json file."),
+    description=("This package provides the user with an easy to use endpoint generator, "
+                 "which that exposes endpoints that are defined through a config JSON file. "
+                 "Additionally, data files can be added and linked to return richer data."),
     license="MIT",
+    url="https://github.com/starlord-daniel/flask-stub-server",
     keywords="python stub server flask",
     long_description=read('README.md'),
+    long_description_content_type="text/markdown",
     classifiers=[
-        "Development Status :: 1 - Pre-Alpha",
-        "Topic :: Server",
-        "License :: OSI Approved :: MIT",
-    ]
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["flask_stub_server", "flask_stub_server/endpoint_gen"],
+    python_requires='>=3.9',
+    install_requires=['Flask>=1.1.2'],
 )
